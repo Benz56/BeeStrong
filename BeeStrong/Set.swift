ee1e80e6e6bcbb0748d0cfd4c67322827e71fcc4
Hala <haalj15@student.sdu.dk>
2020-12-14 10:57:37 +0100

    Made a set

---
+++ BeeStrong/Set.swift
@@ -0,0 +1,22 @@
+//
+//  Set.swift
+//  BeeStrong
+//
+//  Created by Hala Al-Janabi on 14/12/2020.
+//
+
+import Foundation
+
+class set {
+    
+    struct set {
+        var repetitions: Int
+        var weight: Double
+        let uuid = UUID()
+        init(repetitions: Int, weight: Double, uuid:UUID) {
+            self.repetitions = repetitions
+            self.weight = weight
+        }
+    }
+    
+}
