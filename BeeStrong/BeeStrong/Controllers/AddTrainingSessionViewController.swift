cff64b05d717efc7fed9cdf0bb3b442c1b8cbffc
SofieLouise <slm@esoft.com>
2020-12-17 22:03:47 +0100

    Connected storyboard fields with session creation.

--- BeeStrong/BeeStrong/Controllers/AddTrainingSessionViewController.swift
+++ BeeStrong/BeeStrong/Controllers/AddTrainingSessionViewController.swift
@@ -13,7 +13,10 @@ class AddTrainingSessionViewController: UIViewController, UITableViewDelegate, U
     private let workoutManager = WorkoutManager()
     private var allWorkouts: [Workout]?
     @IBOutlet weak var selectedWorkoutsTableView: UITableView!
-    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var titleLabel: UITextField!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var startTimePicker: UIDatePicker!
+    @IBOutlet weak var endTimePicker: UIDatePicker!
     var date: Date?
     let googleCalendarAPI = GoogleCalendarAPI();
     
@@ -21,16 +24,26 @@ class AddTrainingSessionViewController: UIViewController, UITableViewDelegate, U
         super.viewDidLoad()
         selectedWorkoutsTableView.delegate = self
         selectedWorkoutsTableView.dataSource = self
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "dd-MM-YYYY"
-        dateLabel.text = dateFormatter.string(from: date!)
+        /// Give date object the current hour and minute. Otherwise it will always show 1:00 on the time pickers.
+        if let dateWithoutHours = date {
+            let hour = Calendar.current.component(.hour, from: Date())
+            let minute = Calendar.current.component(.minute, from: Date())
+            self.date = Calendar.current.date(bySettingHour: hour, minute: minute, second: 0, of: dateWithoutHours)!
+        } else {
+            self.date = Date()
+        }
+        datePicker.date = date!
+        startTimePicker.date = date!
+        endTimePicker.date = Date(timeInterval: 120*60, since: date!)
+        datePicker.minimumDate = Date()
+        startTimePicker.minimumDate = Date()
+        endTimePicker.minimumDate = Date()
     }
     
     override func viewDidAppear(_ animated: Bool) {
         allWorkouts = workoutManager.getAllWorkouts()
         selectedWorkoutsTableView.reloadData()
     }
-    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return allWorkouts?.count ?? 0
     }
@@ -40,9 +53,7 @@ class AddTrainingSessionViewController: UIViewController, UITableViewDelegate, U
         let workout = allWorkouts?[indexPath.row]
 
         cell.titleLabel.text = workout?.title
-        cell.exercisesLabel.text = workout?.exercises?.array.map{e in e as! Exercise}.map{e in
-            "\(e.exerciseType!.title!) \n   " + e.sets!.array.map{s in s as! WorkingSet}.map{s in "\(s.repetitions) reps @ \(s.weight) kg"}.joined(separator: "\n   ")
-        }.joined(separator: "\n") ?? "?"
+        cell.exercisesLabel.text = workoutToString(workout)
         
         return cell
     }
@@ -52,7 +63,14 @@ class AddTrainingSessionViewController: UIViewController, UITableViewDelegate, U
         var workouts = [Workout]()
         selectedWorkoutsTableView.indexPathsForSelectedRows?.forEach{workouts.append((allWorkouts?[$0.row])!)}
         tsManager.add(for: date!, with: workouts) //TODO set the selected time on the date. It should really be an interval.
-        self.googleCalendarAPI.createEventEndpoint(name: "Training session", description: workouts.description, startDate: date ?? Date(), endDate: Date(timeInterval: 3600, since: date ?? Date()))
+        var sessionDescription = "<h2>Workouts</h2>"
+        for workout in workouts {
+            let workoutTitle = workout.title ?? ""
+            sessionDescription += ("\n<b>" + workoutTitle + "</b>\n")
+            sessionDescription += workoutToString(workout)
+        }
+        let title = titleLabel.text != nil ? titleLabel.text! : "Training session"
+        self.googleCalendarAPI.createEventEndpoint(name: title, description: sessionDescription, startDate: startTimePicker.date, endDate: endTimePicker.date)
         alertOnSave()
     }
     
@@ -60,5 +78,15 @@ class AddTrainingSessionViewController: UIViewController, UITableViewDelegate, U
         let inputAlert = UIAlertController(title: "Saved", message: "Training session is saved.", preferredStyle: .alert)
         inputAlert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
         self.present(inputAlert, animated: true, completion: nil)
+    }
+    
+    fileprivate func workoutToString(_ workout: Workout?) -> String {
+        return workout?.exercises?.array.map{e in e as! Exercise}.map{e in
+            "\(e.exerciseType!.title!) \n   " + e.sets!.array.map{s in s as! WorkingSet}.map{s in "\(s.repetitions) reps @ \(s.weight) kg"}.joined(separator: "\n   ")
+        }.joined(separator: "\n") ?? "?"
+    }
+    
+    @objc func datePickerValueChanged(_ datePicker: UIDatePicker) {
+        endTimePicker.minimumDate = Date(timeInterval: 60, since: startTimePicker.date)
     }
 }
