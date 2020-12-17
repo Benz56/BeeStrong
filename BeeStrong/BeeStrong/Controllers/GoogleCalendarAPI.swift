de15de29ee1002ded6587e6b97c8b5f232957fa2
SofieLouise <slm@esoft.com>
2020-12-17 14:50:14 +0100

    Clean up API

--- BeeStrong/BeeStrong/Controllers/GoogleCalendarAPI.swift
+++ BeeStrong/BeeStrong/Controllers/GoogleCalendarAPI.swift
@@ -19,55 +19,60 @@ class GoogleCalendarAPI {
     /// Creates calendar service with current authentication
     fileprivate lazy var calendarService: GTLRCalendarService? = {
         let service = GTLRCalendarService()
-        // Have the service object set tickets to fetch consecutive pages
-        // of the feed so we do not need to manually fetch them
+        /// Have the service object set tickets to fetch consecutive pages of the feed so we do not need to manually fetch them
         service.shouldFetchNextPages = true
-        // Have the service object set tickets to retry temporary error conditions
-        // automatically
+        /// Have the service object set tickets to retry temporary error conditions automatically
         service.isRetryEnabled = true
         service.maxRetryInterval = 15
         
-        guard let currentUser = GIDSignIn.sharedInstance().currentUser,
-              let authentication = currentUser.authentication else {
-            return nil
-        }
-        
-        service.authorizer = authentication.fetcherAuthorizer()
+        self.authorizeCurrentUser()
         return service
     }()
     
+    func authorizeCurrentUser(){
+        guard let service = self.calendarService,
+              let currentUser = GIDSignIn.sharedInstance().currentUser,
+              let authentication = currentUser.authentication else {
+            return
+        }
+        service.authorizer = authentication.fetcherAuthorizer()
+    }
+    
     func createTrainingCalendar() -> String? {
+        print("Creating training calendar")
         guard let service = self.calendarService else {
+            print("No calendar service found.")
             return nil
         }
-        
+        /// Define calendar properties
         let calendar = GTLRCalendar_Calendar();
         calendar.summary = self.calendarName
         calendar.timeZone = "Europe/Zurich"
+        
+        ///Running the query to the Google Calendar API to insert Calendar
         let query = GTLRCalendarQuery_CalendarsInsert.query(withObject: calendar);
         query.fields = "id"
-        //This is actually running the query you just built
         service.executeQuery(query, completionHandler: { (ticket, result, error) in
             guard error == nil, let calendar = (result as? GTLRCalendar_Calendar) else {
-                print(error)
+                print(error!)
                 return
             }
             self.calendarId = calendar.identifier!
-            print(self.calendarId)
         })
-        return calendarId;
+        return self.calendarId;
     }
     
     func getListOfCalendars() -> [GTLRCalendar_CalendarListEntry]?{
         guard let service = self.calendarService else {
+            print("No calendar service found.")
             return nil
         }
+        
+        /// Querying API for list of calendars
         let query = GTLRCalendarQuery_CalendarListList.query();
-        //This is actually running the query you just built
-        var calendarList : [GTLRCalendar_CalendarListEntry]?;
         service.executeQuery(query, completionHandler : { (ticket, result, error) in
             guard error == nil, let calendars = (result as? GTLRCalendar_CalendarList)?.items else {
-                print(error)
+                print(error!)
                 return
             }
             if calendars.count > 0 {
@@ -77,26 +82,69 @@ class GoogleCalendarAPI {
                     print("")
                 }
             }
-            calendarList = calendars;
+            self.calendarList = calendars;
             
         });
         return calendarList;
     }
     
     func getIdOfTrainingCalendar() -> String? {
-        guard let calendars = self.getListOfCalendars() else {
-            print("Creating event failed. Error with getting list of calendars")
-            return nil;
+        if (self.calendarList == nil){
+            self.getListOfCalendars();
         }
-        for calendar in calendars {
-            if (calendar.summary == self.calendarName) {
-                return calendar.identifier
+        if let calendars = self.calendarList {
+            for calendar in calendars {
+                if (calendar.summary == self.calendarName) {
+                    print("Training calendar found. \(calendar.identifier!)")
+                    self.calendarId = calendar.identifier!
+                    return calendar.identifier!
+                }
             }
         }
+        print("No training calendar found.")
         return self.createTrainingCalendar()
     }
     
-    func createEvent() {
+    func createEvent(name: String, startDate: Date, endDate: Date) {
+        guard let service = self.calendarService else {
+            print("Creating event failed. Error with calendar service")
+            return
+        }
+        
+        let newEvent = GTLRCalendar_Event();
+        newEvent.summary = "Training session 1"
+        
+        ///Start Date
+        let startDateTime: GTLRDateTime = GTLRDateTime(date: startDate)
+        let startEventDateTime: GTLRCalendar_EventDateTime = GTLRCalendar_EventDateTime()
+        startEventDateTime.dateTime = startDateTime
+        newEvent.start = startEventDateTime
+        
+        ///End date
+        let endDateTime: GTLRDateTime = GTLRDateTime(date: endDate)
+        let endEventDateTime: GTLRCalendar_EventDateTime = GTLRCalendar_EventDateTime()
+        endEventDateTime.dateTime = endDateTime
+        newEvent.end = endEventDateTime
+        
+        /// Querying API to insert event with existing
+        let query =
+            GTLRCalendarQuery_EventsInsert.query(withObject: newEvent, calendarId: self.calendarId)
+        
+        //This is the part that I forgot. Specify your fields! I think this will change when you add other perimeters, but I need to review how this works more.
+        query.fields = "id,summary";
+        
+        //This is actually running the query you just built
+        service.executeQuery(query, completionHandler: { (ticket, result, error) in
+            guard error == nil, let event = (result as? GTLRCalendar_Event) else {
+                print(error!)
+                return
+            }
+            print(event.summary!)
+            print(event.identifier!)
+        });
+    }
+    
+    func createTestEvent(name: String, startDate: Date, endDate: Date) {
         guard let service = self.calendarService else {
             print("Creating event failed. Error with calendar service")
             return
@@ -127,11 +175,11 @@ class GoogleCalendarAPI {
         //This is actually running the query you just built
         service.executeQuery(query, completionHandler: { (ticket, result, error) in
             guard error == nil, let event = (result as? GTLRCalendar_Event) else {
-                print(error)
+                print(error!)
                 return
             }
-            print(event.summary)
-            print(event.identifier)
+            print(event.summary!)
+            print(event.identifier!)
         });
     }
 }
