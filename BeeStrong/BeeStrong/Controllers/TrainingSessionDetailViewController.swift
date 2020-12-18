11d136c0bcda7fbc0108a633e18f880b698d66af
SofieLouise <slm@esoft.com>
2020-12-18 18:58:46 +0100

    Added detail view for training session

--- BeeStrong/BeeStrong/Controllers/TrainingSessionDetailViewController.swift
+++ BeeStrong/BeeStrong/Controllers/TrainingSessionDetailViewController.swift
@@ -12,35 +12,47 @@ class TrainingSessionDetailViewController: UIViewController, UITableViewDelegate
     
     let workoutManager = WorkoutManager()
     var workouts: [Workout]?
+    var exercises : [Exercise]?
     
-    @IBOutlet weak var workoutsTableViewCell: UITableView!
+    @IBOutlet weak var workoutsTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        workoutsTableViewCell.delegate = self
-        workoutsTableViewCell.dataSource = self
+        let nib = UINib(nibName: "WorkoutDetailTableViewCell", bundle: nil)
+        workoutsTableView.register(nib, forCellReuseIdentifier: "WorkoutDetailTableViewCell")
+        workoutsTableView.delegate = self
+        workoutsTableView.dataSource = self
     }
     
     override func viewDidAppear(_ animated: Bool) {
         workouts = workoutManager.getAllWorkouts()
-        workoutsTableViewCell.reloadData()
+        exercises = []
+        for workout in workouts! {
+            let workoutExercises = workout.exercises?.array.map{e in e as! Exercise}
+            if(workoutExercises != nil) {
+                exercises?.append(contentsOf: workoutExercises!)
+            }
+        }
+        workoutsTableView.reloadData()
     }
-
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return workouts?.count ?? 0
+        return workouts![section].exercises?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return workouts![section].title
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "workoutDetailsCell")!
-        let workout = workouts?[indexPath.row]
-
-        cell.textLabel?.text = workout?.title
-        /*
-        cell.exercisesLabel.text = workout?.exercises?.array.map{e in e as! Exercise}.map{e in
-            "\(e.exerciseType!.title!) \n   " + e.sets!.array.map{s in s as! WorkingSet}.map{s in "\(s.repetitions) reps @ \(s.weight) kg"}.joined(separator: "\n   ")
-        }.joined(separator: "\n") ?? "?"
-        */
+        let cell = tableView.dequeueReusableCell(withIdentifier: "WorkoutDetailTableViewCell") as! WorkoutDetailTableViewCell
+        let e = workouts?[indexPath.section].exercises?.array.map{e in e as! Exercise}[indexPath.row]
+        cell.title.text = e!.exerciseType!.title! + "\n   " + ((e?.sets!.array.map{s in s as! WorkingSet}.map{s in "\(s.repetitions) reps @ \(s.weight) kg"}.joined(separator: "\n   "))!)
         return cell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return workouts?.count ?? 0
     }
     
 }
